e4279723eb4be24dff3516c534fe73026af90caf
Saketh Vutukuru <sakethvutukuru6@gmail.com>
2023-10-28 17:48:20 -0400

    model evaluation

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -4,6 +4,7 @@ from mlProject.entity.config_entity import DataIngestionConfig
 from mlProject.entity.config_entity import DataValidationConfig
 from mlProject.entity.config_entity import DataTransformationConfig
 from mlProject.entity.config_entity import ModelTrainerConfig
+from mlProject.entity.config_entity import ModelEvaluationConfig
 
 
 
@@ -71,13 +72,14 @@ class ConfigurationManager:
         schema =  self.schema.TARGET_COLUMN
         create_directories([config.root_dir])
         model_trainer_config = ModelTrainerConfig(
-        root_dir=config.root_dir,
-        train_data_path = config.train_data_path,
-        test_data_path = config.test_data_path,
-        model_name = config.model_name,
-        alpha = params.alpha,
-        l1_ratio = params.l1_ratio,
-        target_column = schema.name       
+
+            root_dir=config.root_dir,
+            train_data_path = config.train_data_path,
+            test_data_path = config.test_data_path,
+            model_name = config.model_name,
+            alpha = params.alpha,
+            l1_ratio = params.l1_ratio,
+            target_column = schema.name       
         )
         return model_trainer_config
     
@@ -89,14 +91,13 @@ class ConfigurationManager:
         create_directories([config.root_dir])
 
         model_evaluation_config = ModelEvaluationConfig(
+
             root_dir=config.root_dir,
             test_data_path=config.test_data_path,
             model_path = config.model_path,
             all_params=params,
             metric_file_name = config.metric_file_name,
             target_column = schema.name,
-            mlflow_uri="https://dagshub.com/SakethVutukuru/end-to-end-machine-learning-project-with-MLflow.mlflow",
-           
+            mlflow_uri="https://dagshub.com/SakethVutukuru/end-to-end-machine-learning-project-with-MLflow.mlflow"  
         )
-
         return model_evaluation_config
