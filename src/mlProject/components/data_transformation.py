4ac7d02e515ac189401cfb00256cff4a5e9cc31e
Saketh Vutukuru <sakethvutukuru6@gmail.com>
2023-10-28 14:15:45 -0400

    changes to data data_transformation

--- src/mlProject/components/data_transformation.py
+++ src/mlProject/components/data_transformation.py
@@ -1,9 +1,9 @@
 import os
+import pandas as pd
 from mlProject import logger
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-import pandas as pd
 from mlProject.entity.config_entity import DataTransformationConfig
 
 class DataTransformation:
@@ -14,52 +14,42 @@ class DataTransformation:
         # Load the data
         data = pd.read_csv(self.config.data_path)
 
+        # Shuffle the data
+        data = data.sample(frac=1, random_state=42).reset_index(drop=True)
+
+        # Define the target variable
+        target_variable = 'quality'
+
+        # Split the data into features (X) and the target variable (y)
+        X = data.drop(target_variable, axis=1)
+        y = data[target_variable]
+
         # Split the data into training and test sets with randomization (0.75, 0.25) split.
-        train, test = train_test_split(data, test_size=0.25, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
-        # Apply StandardScaler
+        # Apply StandardScaler only to the features (X)
         scaler = StandardScaler()
-        train_scaled = scaler.fit_transform(train)
-        test_scaled = scaler.transform(test)
+        X_train_scaled = scaler.transform(X_train)
+        X_test_scaled = scaler.transform(X_test)
 
-        # Apply PCA (Principal Component Analysis)
-        #pca = PCA(n_components=2)  # You can specify the number of components as needed
-        #train_pca = pca.fit_transform(train_scaled)
-        #test_pca = pca.transform(test_scaled)
+        # Apply PCA (Principal Component Analysis) if needed
+        # pca = PCA(n_components=2)  # You can specify the number of components as needed
+        # X_train_pca = pca.fit_transform(X_train_scaled)
+        # X_test_pca = pca.transform(X_test_scaled)
 
         # Save the transformed data
-        train_scaled = pd.DataFrame(train_scaled)
-        test_scaled = pd.DataFrame(test_scaled)
+        train_data = pd.DataFrame(X_train_scaled, columns=X.columns)
+        test_data = pd.DataFrame(X_test_scaled, columns=X.columns)
 
-        train_scaled.to_csv(os.path.join(self.config.root_dir, "train.csv"), index=False)
-        test_scaled.to_csv(os.path.join(self.config.root_dir, "test.csv"), index=False)
+        train_data[target_variable] = y_train
+        test_data[target_variable] = y_test
 
-        logger.info("Transformed data with StandardScaler and PCA and saved to CSV files with randomization")
-        logger.info(train.shape)
-        logger.info(test.shape)
+        train_data.to_csv(os.path.join(self.config.root_dir, "train.csv"), index=False)
+        test_data.to_csv(os.path.join(self.config.root_dir, "test.csv"), index=False)
 
-        print(train.shape)
-        print(test.shape)
+        logger.info("Transformed data with StandardScaler and saved to CSV files with randomization")
+        logger.info(train_data.shape)
+        logger.info(test_data.shape)
 
-'''
-class DataTransformation:
-    def __init__(self, config: DataTransformationConfig):
-        self.config = config
-
-
-    def train_test_spliting(self):
-        data = pd.read_csv(self.config.data_path)
-
-        # Split the data into training and test sets. (0.75, 0.25) split.
-        train, test = train_test_split(data)
-
-        train.to_csv(os.path.join(self.config.root_dir, "train.csv"),index = False)
-        test.to_csv(os.path.join(self.config.root_dir, "test.csv"),index = False)
-
-        logger.info("Splited data into training and test sets")
-        logger.info(train.shape)
-        logger.info(test.shape)
-
-        print(train.shape)
-        print(test.shape)
-'''
+        print(train_data.shape)
+        print(test_data.shape)
